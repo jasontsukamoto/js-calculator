123acac4634c17ab3677bfc3641f7b4c9bf2bfbc
Jason Tsukamoto <jasontsukamoto@gmail.com>
2015-06-19 09:50:48 -1000

    all tests pass

--- calculator.js
+++ calculator.js
@@ -5,31 +5,31 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
- var calculatorModule = function(number) {
-   var memory = 0;
-   var total = 0;
-   return {
-     load: _load,
-     getTotal: _getTotal,
-     add: _adds,
-     subtract: _subtract,
-     multiply: _multiply,
-     divide: _divide,
-     recallMemory: _recallMemory,
-     saveMemory: _saveMemory,
-     clearMemory: _clearMemory
-  }
+var calculatorModule = function(number) {
+  var memory = 0;
+  var total = 0;
+  return {
+    load : _load,
+    getTotal : _getTotal,
+    add : _adds,
+    subtract : _subtract,
+    multiply : _multiply,
+    divide : _divide,
+    recallMemory : _recallMemory,
+    saveMemory : _saveMemory,
+    clearMemory : _clearMemory
+  };
 
-/**
-* sets the `total` to the number passed in
-* @param  { Number } x
-* @return { Number }    current total
-*/
-  function _load(number){
+  /**
+  * sets the `total` to the number passed in
+  * @param  { Number } x
+  * @return { Number }    current total
+  */
+  function _load(number) {
     if (typeof number !== 'number') {
       return 'Need a number';
     } else {
-       total = number;
+      total = number;
       return total;
     }
   }
@@ -61,20 +61,20 @@
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-   function _subtract(number) {
+  function _subtract(number) {
     if (typeof number !== 'number') {
       return 'Need a number';
     } else {
       total -= number;
     }
-   }
+  }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-   function _multiply(number) {
-   if (typeof number !== 'number') {
+  function _multiply(number) {
+    if (typeof number !== 'number') {
       return 'Need a number';
     } else {
       total *= number;
@@ -85,7 +85,7 @@
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-   function _divide(number) {
+  function _divide(number) {
     if (typeof number !== 'number') {
       return 'Need a number';
     } else {
@@ -97,24 +97,24 @@
    * Return the value stored at `memory`
    * @return { Number }
    */
-   function _recallMemory() {
+  function _recallMemory() {
     return memory;
-   }
+  }
 
 
   /**
    * Stores the value of `total` to `memory`
    */
-   function _saveMemory() {
+  function _saveMemory() {
     memory = total;
-   }
+  }
 
   /**
    * Clear the value stored at `memory`
    */
-   function _clearMemory() {
+  function _clearMemory() {
     memory = 0;
-   }
+  }
 
   /**
    * Validation
